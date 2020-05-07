9f73e600187c8d9a4f4d6119f7336a5d0b3a402c
liubr404 <liubr1996@outlook.com>
2020-05-07 00:12:38 -0700

    update stack practice

--- MyStack.cs
+++ MyStack.cs
@@ -1,60 +1,57 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Linq.Expressions;
-
-public class MyStack
-{
-    private List<int> stack = new List<int>();
-
-    public void Push(int num)
-    {
-        stack.Add(num);
-    }
-
-    public int Pop()
-    {
-        if (stack.Count == 0)
-        {
-            throw new IndexOutOfRangeException();
-        }
-        else {
-	    int element = stack[stack.Count - 1];
-            stack.RemoveAt(stack.Count - 1);
-            return element;
-        }
-    }
-
-    public int Peek()
-    {
-        if (stack.Count == 0)
-        {
-            throw new IndexOutOfRangeException();
-        }
-        else
-        {
-            return stack[stack.Count - 1];
-        }
-    }
-}
 
 namespace ConsoleApp1
-
 {
-    class Program
+    public class MyStack
     {
-        static void Main(string[] args)
-        {
-            MyStack myStack = new MyStack();
-            myStack.Push(10);
-            myStack.Push(11);
-            myStack.Push(12);
-            myStack.Push(13);
+        private List<int> stack = new List<int>();
 
-            Console.WriteLine("peek: " + myStack.Peek());
-            //pop
-            myStack.Pop();
-            Console.WriteLine("new peek: " + myStack.Peek());
+        public void Push(int num)
+        {
+            stack.Add(num);
+        }
+
+        public int Pop()
+        {
+            if (stack.Count == 0)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            else
+            {
+                int element = stack[stack.Count - 1];
+                stack.RemoveAt(stack.Count - 1);
+                return element;
+            }
+        }
+
+        public int Peek()
+        {
+            if (stack.Count == 0)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            else
+            {
+                return stack[stack.Count - 1];
+            }
+        }
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                MyStack myStack = new MyStack();
+                myStack.Push(10);
+                myStack.Push(11);
+                myStack.Push(12);
+                myStack.Push(13);
+
+                Console.WriteLine("peek: " + myStack.Peek());
+                //pop
+                myStack.Pop();
+                Console.WriteLine("new peek: " + myStack.Peek());
+            }
         }
     }
 }
