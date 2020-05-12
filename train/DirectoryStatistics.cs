0a036c93e1f2c0a11fd006a497c91156696956bb
liubr404 <liubr1996@outlook.com>
2020-05-12 00:03:29 -0700

    combine daily practice

---
+++ train/DirectoryStatistics.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace DirectoryStatistics
+{
+    class day5
+    {
+        public static long length;
+        public static double size;
+        public static void Main(string[] args)
+        {
+            /*
+            #if DEBUG
+                        args = new[] { "-d /Users/admin/Desktop/daily report/first_day.txt -p *.txt" };
+            #endif
+            */
+            string filePath;
+            string pattern;
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            if (args.Length > 0 && args[0] == "-d" && args[2] == "-p")
+            {
+                filePath = args[1];
+                pattern = args[3];
+                Console.WriteLine("File name pattern: {0}", pattern);
+                /*for (int i = 0; i < args.Length; i++) {
+                    Console.WriteLine(args[i]);
+                }*/
+                //debug
+                string[] files = Directory.GetFiles(filePath, pattern, SearchOption.AllDirectories);
+                if (files.Length == 0)
+                {
+                    throw new Exception("No suitable file");
+                }
+                else
+                {
+                    List<string> fileNames = new List<string>();
+                    int fileCount = files.Length;
+                    //find all pattern files in the folder
+                    foreach (string file in files)
+                    {
+                        FileInfo info = new FileInfo(file);
+                        length += info.Length;
+                        //calculate the size of current file
+                        /*
+                        pattern = "+" + pattern.Remove(0, 1);
+                        MatchCollection match = Regex.Matches(file, @"[\w+$]" + pattern);
+                        // match all .txt files.
+                        foreach (Match matchFiles in match)
+                        {
+                            fileNames.Add(matchFiles.Groups[0].Value);
+                            //Console.WriteLine(matchFiles.Groups[0].Value);
+                            //debug
+                        }
+                        */
+                        //this method using regex to get all txt name from the path
+                        fileNames.Add(file.Replace(filePath, ".."));
+                    }
+                    size = (length / 1024f) / 1024f;
+                    watch.Stop();
+                    Console.WriteLine("File count:{0}\nTotal size:{1} MB({2} bytes)\nTime elapsed: {3} ms\nFile list:", fileCount, size, length, watch.ElapsedMilliseconds);
+                    for (int i = 0; i < fileNames.Count; i++)
+                    {
+                        Console.WriteLine("--{0}", fileNames[i]);
+                    }
+                }
+            }
+            else
+            {
+                throw new Exception("pls input in correct format");
+            }
+            //Console.WriteLine(filePath, pattern);
+        }
+    }
+}
---
+++ train/File.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+class Day3
+{
+    static void Main()
+    {
+        Console.WriteLine(CountInTextFile("/Users/admin/Desktop/daily report/first_day.txt", true, true, true).LineCount);
+        FilterLinesWithNoNumbers("/Users/admin/Desktop/daily report/first_day.txt", "/Users/admin/Desktop/daily report/first_day_temp.txt");
+    }
+
+    public struct FileCount
+    {
+        public string filePath;
+        public int LineCount;
+        public long CharCount;
+        public int WrodCount;
+    }
+
+    public static FileCount CountInTextFile(string filePath, bool countLines, bool countWords, bool countChars)
+    {
+        if (!System.IO.File.Exists(filePath))
+        {
+            throw new FileNotFoundException();
+        }
+        //whether the file exist
+        int lines = 0, words = 0;
+        long chars = 0;
+        using (StreamReader r = new StreamReader(filePath))
+        {
+            string sentence;
+            // save each line
+            while ((sentence = r.ReadLine()) != null)
+            {
+                //Console.WriteLine(sentence);
+                lines++;
+                string[] word = sentence.Split(' ');
+                words += word.Length;
+                chars += sentence.Length;
+            }
+        }
+        FileCount result = new FileCount();
+        result.filePath = filePath;
+        // read one line each iteration so that use less memory.
+        if (countLines)
+        {
+            result.LineCount = lines;
+            Console.WriteLine("The number of lines in this file is " + lines);
+        }
+        if (countWords)
+        {
+            result.WrodCount = words;
+            Console.WriteLine("The number of words in this file is " + words);
+        }
+        if (countChars)
+        {
+            result.CharCount = chars;
+            Console.WriteLine("The number of chars in this file is " + chars);
+        }
+        return result;
+    }
+
+    public static void FilterLinesWithNoNumbers(string filePath, string destinationFilePath)
+    {
+        if (!System.IO.File.Exists(filePath))
+        {
+            throw new FileNotFoundException();
+        }
+        using (StreamReader reader = new StreamReader(filePath))
+        {
+            using (StreamWriter writer = new StreamWriter(destinationFilePath))
+            {
+                string sentence;
+                Regex regex = new Regex(@".*\d+.*", RegexOptions.RightToLeft);
+                while ((sentence = reader.ReadLine()) != null)
+                {
+                    if (regex.Match(sentence).Success)
+                        continue;
+                    // if the line contains number,go to next iteration.
+                    writer.WriteLine(sentence);
+                }
+            }
+        }
+    }
+}
