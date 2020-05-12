e107be6072113becb5ac253bb472167585a06fe3
liubr404 <liubr1996@outlook.com>
2020-05-12 00:00:02 -0700

    get directory information and run exe file

---
+++ runExe.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace RunExe
+{
+    class Program
+    {
+        static void Main()
+        {
+            var proc = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "c://users/admin/source/repos/DirectoryStatistics/DirectoryStatistics/bin/Debug/netcoreapp3.1/DirectoryStatistics.exe",
+                    Arguments = "-d /Users/admin/Desktop/dailyreport -p *.txt",
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    CreateNoWindow = true
+                }
+            };
+            proc.Start();
+            while (!proc.StandardOutput.EndOfStream)
+            {
+                string line = proc.StandardOutput.ReadLine();
+                // do something with line
+                Console.WriteLine(line);
+            }
+        }
+    }
+}
