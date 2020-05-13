79958690f9f5a5b77001338a3afd5802958446e3
liubr404 <liubr1996@outlook.com>
2020-05-12 22:53:18 -0700

    min edit distance and test function

---
+++ train/MED.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace MinimumEditDistance
+{
+    public class program
+    {
+        public const int Errnum = -1;
+        public static void Main()
+        {
+            Console.WriteLine("debugging");
+        }
+        public static int MED(string str1, string str2)
+        {
+            if (str1 == null && str2 == null)
+            {
+                Console.WriteLine("The input string is empty");
+                return Errnum;
+            }
+            else
+            {
+                int[,] table = new int[str1.Length + 1, str2.Length + 1];
+                for (int i = 0; i < str1.Length + 1; i++)
+                {
+                    for (int j = 0; j < str2.Length + 1; j++)
+                    {
+                        //the length should be string length + 1!
+                        if (i == 0)
+                        {
+                            table[i, j] = j;
+                        }
+                        else if (j == 0)
+                        {
+                            table[i, j] = i;
+                        }
+                        //set the frist row and column
+                        else if (str1[i - 1] == str2[j - 1])
+                        {
+                            table[i, j] = table[i - 1, j - 1];
+                        }
+                        //if the char in i-1 is the same as char in j-1, there is no operation needed
+                        else
+                        {
+                            table[i, j] = Math.Min(
+                                Math.Min(
+                                    table[i - 1, j] + 1,
+                                    //delete str1[i]
+                                    table[i, j - 1] + 1
+                                //insert str2[j] to str1
+                                ), table[i - 1, j - 1] + 1);
+                            //substitute str1[i] by str2[j]
+                        }
+                    }
+                }
+                //Console.WriteLine("The distance between {0} and {1} is {2}", str1, str2, table[str1.Length - 1, str2.Length - 1]);
+                //debug
+                return table[str1.Length, str2.Length];
+            }
+        }
+    }
+}
---
+++ train/test.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using System.IO;
+using System;
+
+namespace MED_test
+{
+    public class Tests
+    {
+        private int[] Expected = new int[] { 5, 1, 4, 4 };
+        private string[] str1 = new string[] { "intention", "abc", "hello", "lion" };
+        private string[] str2 = new string[] { "execution", "dbc", "hi", "emotion" };
+        [SetUp]
+        public void Setup()
+        {
+        }
+        [Test]
+        public void TestMethod1()
+        {
+            for (int i = 0; i < str1.Length; i++)
+            {
+                int result = MinimumEditDistance.program.MED(str1[i], str2[i]);
+                Assert.AreEqual(Expected[i], result);
+            }
+        }
+    }
+}
