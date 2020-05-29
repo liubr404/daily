592356795a996fa16708187b0f49837aa31a10a0
liubr404 <liubr1996@outlook.com>
2020-05-29 15:20:49 +0800

    leetcode 2_keys_keyboard

---
+++ leetcode/2_keys_keyboard.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Leetcode
+{
+    class Program
+    {
+        public class Solution
+        {
+            public static int MinSteps(int n)
+            {
+                if (n == 1)
+                {
+                    return 0;
+                }
+                int a = 0;
+                for (int i = 2; n > 1; i++)
+                {
+                    while (n % i == 0)
+                    {
+                        a += i;
+                        n /= i;
+                    }
+                }
+                return a;
+            }
+
+            public static void Main(string[] args)
+            {
+                Console.WriteLine(MinSteps(4));
+            }
+        }
+    }
+}
