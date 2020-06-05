ba66f103681fe927b8dd015823a9731b2067a508
liubr404 <liubr1996@outlook.com>
2020-06-05 15:59:55 +0800

    leetcode Top K Frequent

---
+++ leetcode/TopKFrequent.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Leetcode
+{
+    class Program
+    {
+        public class Solution
+        {
+            public static IList<string> TopKFrequent(string[] words, int k)
+            {
+                Dictionary<string, int> count = new Dictionary<string, int>();
+                IList<string> result = new List<string>();
+                int temp = 0;
+                for (int i = 0; i < words.Length; i++)
+                {
+                    if (count.ContainsKey(words[i]))
+                    {
+                        count[words[i]] += 1;
+                    }
+                    else
+                    {
+                        count.Add(words[i], 1);
+                    }
+                }
+                var orderedDict = count.OrderByDescending(x => x.Value).ThenBy(x => x.Key);
+                foreach (KeyValuePair<string, int> pair in orderedDict)
+                {
+                    if (temp < k)
+                    {
+                        result.Add(pair.Key);
+                    }
+                    else
+                    {
+                        break;
+                    }
+                    temp++;
+                }
+                return result;
+            }
+
+
+            public static void Main(string[] args)
+            {
+                string[] words = { "i", "love", "leetcode", "i", "love", "coding" };
+                IList<string> result = new List<string>();
+                result = TopKFrequent(words, 2);
+                for (int i = 0; i < result.Count; i++)
+                {
+                    Console.WriteLine(result[i]);
+                }
+            }
+        }
+    }
+}
