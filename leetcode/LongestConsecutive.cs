9677b350c1f69df5cb52605bda0e7745c4523cb8
liubr404 <liubr1996@outlook.com>
2020-06-06 16:12:52 +0800

    faster way for LongestConsecutive problem

--- leetcode/LongestConsecutive.cs
+++ leetcode/LongestConsecutive.cs
@@ -8,18 +8,34 @@ namespace Leetcode
     {
         public class Solution
         {
-            public static int LongestConsecutive(int[] nums)
+            public static int LongestConsecutive(int[] nums_)
             {
+                // Dictinary find is O(1) operation while list or array is O(n)
+                // Remove duplicates
+                nums_ = nums_.Distinct().ToArray();
+                // save in Dictionary(or hashset)
+                Dictionary<int, int> nums = new Dictionary<int, int>();
+                foreach (int num in nums_)
+                {
+                    if (nums.ContainsKey(num))
+                    {
+                        nums[num] += 1;
+                    }
+                    else
+                    {
+                        nums.Add(num, 1);
+                    }
+                }
                 int temp_len = 0;
                 int temp_num = 0;
                 int Max_len = 0;
-                foreach (int num in nums)
+                foreach (int num in nums.Keys)
                 {
-                    if (!nums.Contains(num - 1))
+                    if (!nums.Keys.Contains(num - 1))
                     {
                         temp_len += 1;
                         temp_num = num;
-                        while (nums.Contains(temp_num + 1))
+                        while (nums.Keys.Contains(temp_num + 1))
                         {
                             temp_num += 1;
                             temp_len += 1;
